d37875120158355febb3ceffef8ecef8becd6387
ValeriaGrenkova <zavdv778@gmail.com>
2022-02-24 23:48:37 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -10,3 +10,16 @@ $order = &$very_bad_unclear_name;
 $order .= " and sauce";
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+//№2
+$number1 = 8;
+echo $number1."<br>";
+$number2 = 30;
+echo $number2."<br>";
+$number3 = 8.303;
+echo $number3."<br>";
+echo ($number1 + 4)."<br>";
+
+
+
+
