b648cdb2faa2b42e25314efcc73fac01f2ef6323
ValeriaGrenkova <zavdv778@gmail.com>
2022-02-25 17:48:07 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -28,7 +28,8 @@ echo ($last_month - $this_month)."<br>";
 $num_languages = 4;
 $months = 11;
 $days = $months * 16;
-$days_per_language = $days / $num_languages
+$days_per_language = $days / $num_languages;
+echo $days_per_language."<br>";
 
 
 
