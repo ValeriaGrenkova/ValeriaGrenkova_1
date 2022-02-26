572b267d8bd93f2cc313891b046248c1bc37aa80
ValeriaGrenkova <zavdv778@gmail.com>
2022-02-26 15:09:18 +0200

    Update num1.php

--- code/lab3/Num1/num1.php
+++ code/lab3/Num1/num1.php
@@ -7,3 +7,13 @@ $s = [];
 $count = preg_match_all($k, $str, $s);
 print_r($s);
 echo "<br>";
+
+//#1_b
+$str = 'a1b2c3';
+echo preg_replace_callback('#(\d+)#', 'cube' , $str);
+function kub_chisla($k)
+{
+	$k[0] = $k[0] ** 3;
+	return $k[0];
+}
+
